1ca78e90405977f4a410ed308dbeeb3d9d478694
niksmac <hello@nikhilmohan.in>
2016-12-28 21:26:05 +0530

    Writing geojson to blockchain as txns

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-require('locus');
-// var parseJson = require('parse-json');
-var jsonfile = require('jsonfile')
+var Web3 = require('web3')
+var jsonfile = require('jsonfile');
 
-var file = 'data/dubai_urban_boundary.geojson.json'
+if (typeof web3 !== 'undefined') {
+  web3 = new Web3(web3.currentProvider);
+} else {
+  web3 = new Web3(new Web3.providers.HttpProvider("http://eth.lightrains.com"));
+}
+
+var file = 'data/simple.geojson.json'
 jsonfile.readFile(file, function(err, obj) {
-  // console.dir(obj.features)
-  eval(locus);
+  for (var i = 0; i < obj.features.length; i++) {
+    var feature = obj.features[i];
+    saveGeojsonFeature(feature);
+  }
 })
+
+function saveGeojsonFeature (info) {
+  var txhash = web3.eth.sendTransaction({
+    from: process.env.COINBASE,
+    to: process.env.TOADDRES,
+    value: 0,
+    data: web3.toHex(info)
+  });
+  console.log(txhash);
+}
