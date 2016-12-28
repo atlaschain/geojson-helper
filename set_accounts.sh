a3593a1b8729a0588b3a96a39da7220b231349ef
niksmac <hello@nikhilmohan.in>
2016-12-28 21:27:25 +0530

    Added config script

---
+++ set_accounts.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+export COINBASE='';
+export TOADDRES='';
+echo "Address set!"
+
+
+# **Accounts**
+#
+#  - 0x239dac47d0b44b413e10973a97966479fd452cfa   123   coinbase
+#  - 0xc2dcea2ebd99e757b07b9f9bb3ec78263e9395f4   123
+#  - 0x42ade140ed0eb22dfddedb0c4623b84753ee29b3   123
+#  - 0xfd2fe758576a0fd9007adbd84c3de14dc1773e16   123
