9a474629f6f14bf4c5ed97de2d8fcea9ca668b60
niksmac <hello@nikhilmohan.in>
2017-01-04 10:51:22 +0530

    Saving and retrieving from txn

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var jsonfile = require('jsonfile');
 if (typeof web3 !== 'undefined') {
   web3 = new Web3(web3.currentProvider);
 } else {
-  web3 = new Web3(new Web3.providers.HttpProvider("http://eth.lightrains.com"));
+  web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.27.101:8545"));
+  // web3 = new Web3(new Web3.providers.HttpProvider("http://eth.lightrains.com"));
 }
 
 var file = 'data/simple.geojson.json'
@@ -19,8 +20,15 @@ function saveGeojsonFeature (info) {
   var txhash = web3.eth.sendTransaction({
     from: process.env.COINBASE,
     to: process.env.TOADDRES,
-    value: 0,
+    value: 1111,
     data: web3.toHex(info)
   });
   console.log(txhash);
+  readGeojsonFeature(txhash);
+}
+
+function readGeojsonFeature(txhash) {
+  web3.eth.getTransaction(txhash, function(err, log) {
+    console.log(web3.toAscii(log.input));
+  })
 }
